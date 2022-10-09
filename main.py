e65af0592c639a6300d9456c61c4c9b1a0b4a86e
linho <linho301150@gmail.com>
2022-10-10 01:51:56 +0900

    Feat: connect static folder

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from app.sample.controller import sample_controller
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(sample_controller.router)
