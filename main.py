c9e58d9974ce062425714f728f48becadea731df
linho <linho301150@gmail.com>
2022-10-11 16:42:01 +0900

    Feat: Setting Health-Check Api

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from typing import Union
-
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from datetime import datetime
 from app.sample.controller import sample_controller
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(sample_controller.router)
+
+
+@app.get("/health-check", tags=["Health-check"], summary="Response DateTime for Health-Check")
+async def health_check():
+    return {"time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")}
