5c2fba3eb2417ed084899a5863179cf4be08528c
linho <linho301150@gmail.com>
2022-10-11 14:36:21 +0900

    Init: Setting dotenv & load sample

--- app/sample/controller/sample_controller.py
+++ app/sample/controller/sample_controller.py
@@ -1,12 +1,20 @@
 from typing import Optional
+from functools import lru_cache
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from pydantic import BaseModel
+from core.config import config
 
 router = APIRouter(
     prefix="/samples",
     tags=["samples"],
     responses={404: {"description": "Not found"}}, )
+
+
+@lru_cache()
+def getConfig():
+    # lru_cache is read env file only once
+    return config.Settings()
 
 
 class Item(BaseModel):
@@ -16,16 +24,25 @@ class Item(BaseModel):
     sex: Optional[str] = None
 
 
-@router.post("/", response_model=Item, tags=["samples"], summary=["this is sample api"])
+@router.post("/", response_model=Item, tags=["samples"], summary='this is sample api')
 async def sample(item: Item, request: Request) -> Item:
     # todo: make get api for sample
 
     """ This is Just Sample Code for post api with Google docstring
     Args:
         item: Request name, description, age, sex
-
+        request (): Get body type
     Returns:
         return input name & description & age & sex
     """
     result: Item = await request.json()
     return result
+
+
+@router.get("/")
+async def get_config_setting(settings: config.Settings = Depends(getConfig)):
+    return {
+        "app_name": settings.app_name,
+        "items_per_user": settings.items_per_user,
+        "admin_email": settings.admin_email,
+    }
